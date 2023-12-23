a605006c5503796d7501f42059b9a000dbfc606e
AllRedCat <leonismogabriel@gmail.com>
2023-12-23 15:20:15 -0300

    style home text

--- portfolio-redcat/src/Home.tsx
+++ portfolio-redcat/src/Home.tsx
@@ -8,25 +8,29 @@ function ContactButton() {
 
 function Home() {
     return (
-        <div className="presentation">
-            <h5>Olá</h5>
-            <h2>Eu sou Gabriel</h2>
-            <div className="redcat">
-                <h4>RedCat</h4>
-                <span className="material-symbols-outlined line">
-                    minimize
-                </span>
+        <div className="hi-container">
+            <div className="hi">
+                <div className="hello">
+                    <h5>Olá</h5>
+                </div>
+                <h2>Eu sou Gabriel</h2>
+                <div className="redcat">
+                    <h4>RedCat</h4>
+                    <span className="material-symbols-outlined line">
+                        minimize
+                    </span>
+                </div>
+                <div className="designer">
+                    <span className="material-symbols-outlined line">
+                        minimize
+                    </span>
+                    <h4>Designer</h4>
+                </div>
+                <div className="button-space">
+                    <ContactButton />
+                </div>
             </div>
-            <div className="designer">
-                <span className="material-symbols-outlined line">
-                    minimize
-                </span>
-                <h4>Designer</h4>
-            </div>
-            <div className="button-space">
-                <ContactButton />
-            </div>
-
+            <div className='my-img'></div>
         </div>
     );
 }
