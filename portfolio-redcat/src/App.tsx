38f6ef06fac250e30b9cda756b897d28d9111df3
AllRedCat <leonismogabriel@gmail.com>
2023-12-23 21:54:23 -0300

    add funciton to show home page

--- portfolio-redcat/src/App.tsx
+++ portfolio-redcat/src/App.tsx
@@ -3,47 +3,42 @@ import './App.css'
 import Home from './Home.tsx'
 import About from './About.tsx'
 
-function NavBar() {
-  return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container bg-ligh-subtle">
-        <a className="navbar-brand" href="#">RedCat</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ms-auto">
-            <a className="nav-link" href="">Sobre</a>
-            <a className="nav-link" href="">Serviços</a>
-            <a className="nav-link" href="">Portfólio</a>
-            <a className="nav-link" href="">Contato</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-}
-
-function Content() {
-  return (
-    <>
-      <Home />
-      <About />
-    </>
-  );
+function HomePage() {
+  return (<Home />);
 }
 
 function App() {
+  const [showHome, setShowHome] = useState(true);
+  
+
+  function HomeShow() {
+    setShowHome(true);
+  }
 
   return (
     <>
       <body>
         <header>
-          <NavBar />
+          <nav className="navbar navbar-expand-lg">
+            <div className="container bg-ligh-subtle">
+              <a className="navbar-brand" href="#" onClick={HomeShow}>RedCat</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav ms-auto">
+                  <a className="nav-link" href="">Sobre</a>
+                  <a className="nav-link" href="">Serviços</a>
+                  <a className="nav-link" href="">Portfólio</a>
+                  <a className="nav-link" href="">Contato</a>
+                </div>
+              </div>
+            </div>
+          </nav>
         </header>
         <main>
-          <Content />
+          {showHome && <HomePage />}
         </main>
       </body>
     </>
