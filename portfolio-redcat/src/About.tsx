04085c96a363f2cf1eba27fae6d17401f07478f4
AllRedCat <leonismogabriel@gmail.com>
2023-12-24 00:42:15 -0300

    need to ajust style sheet

--- portfolio-redcat/src/About.tsx
+++ portfolio-redcat/src/About.tsx
@@ -1,9 +1,13 @@
 import './About.scss'
+import notebookImg from './assets/notebook.svg'
+import pencil from './assets/design.png'
+import graphicDesign from './assets/vector.png'
 
 function About() {
     return (
         <section id="about" className="about-container">
         <div className="my-img-2">
+            <img src={notebookImg} alt="about illustration"/>
         </div>
         <div className="about">
             <h2>Sobre mim</h2>
@@ -13,7 +17,9 @@ function About() {
                     mídia.</h4>
             </div>
             <div className="ilust-container">
+                <img src={pencil} className='ilus' />
                 <i className="bi bi-code-slash"></i>
+                <img src={graphicDesign} className='ilus' />
             </div>
         </div>
     </section>
