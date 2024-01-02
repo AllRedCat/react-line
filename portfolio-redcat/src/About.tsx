454f14f45293adb5a172651a0603ae4ead2737c5
guerson345 <leonismogabriel@gmail.com>
2024-01-02 20:26:54 -0300

    fortating some files and change about's background color

--- portfolio-redcat/src/About.tsx
+++ portfolio-redcat/src/About.tsx
@@ -6,23 +6,23 @@ import graphicDesign from './assets/vector.png'
 function About() {
     return (
         <section id="about" className="about-container">
-        <div className="my-img-2">
-            <img src={notebookImg} alt="about illustration"/>
-        </div>
-        <div className="about">
-            <h2>Sobre mim</h2>
-            <div className="about-text">
-                <h4>Sou Designer Gráfico e Web Designer, trabalho com
-                    soluções de Design para web sites e social
-                    mídia.</h4>
+            <div className="my-img-2">
+                <img src={notebookImg} alt="about illustration" />
             </div>
-            <div className="ilust-container">
-                <img src={pencil} className='ilus' />
-                <i className="bi bi-code-slash"></i>
-                <img src={graphicDesign} className='ilus' />
+            <div className="about">
+                <h2>Sobre mim</h2>
+                <div className="about-text">
+                    <h4>Sou Designer Gráfico e Web Designer, trabalho com
+                        soluções de Design para web sites e social
+                        mídia.</h4>
+                </div>
+                <div className="ilust-container">
+                    <img src={pencil} className='ilus' />
+                    <i className="bi bi-code-slash"></i>
+                    <img src={graphicDesign} className='ilus' />
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
 
