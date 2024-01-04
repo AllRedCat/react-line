d11bb2dd273b218bb2f1fcc4730d0acecbbe6cb6
guerson345 <leonismogabriel@gmail.com>
2024-01-04 01:48:18 -0300

    start styling form

--- portfolio-redcat/src/Home.tsx
+++ portfolio-redcat/src/Home.tsx
@@ -9,23 +9,23 @@ function Home() {
 
     // Botão responsavel pela troca de paginas
     // Nota: Configurar estilo para melhor reutilização
-    function ContactButton() {
-
-        function SwitchScreen() {
-            if (showHome) {
-                setShowHome(false)
-                setShowForm(true)
-            }
-            else {
-                setShowHome(true)
-                setShowForm(false)
-            }
+    function SwitchScreen() {
+        if (showHome) {
+            setShowHome(false)
+            setShowForm(true)
         }
-
-        return (
-            <button onClick={SwitchScreen} className="contact-btn">Contate-me</button>
-        );
+        else {
+            setShowHome(true)
+            setShowForm(false)
+        }
     }
+    // function ContactButton() {
+
+
+    //     return (
+    //         <button onClick={SwitchScreen} className="contact-btn">Contate-me</button>
+    //     );
+    // }
 
     // Aqui vai o formulário
     function FormPage() {
@@ -53,17 +53,20 @@ function Home() {
         };
 
         return (
-            <form onSubmit={onSubmit}>
-                <label>Nome
-                    <input type="text" name="name" />
-                </label>
-                <label>Email
-                    <input type="email" name="email" />
-                </label>
-                <textarea name="message"></textarea>
-                <button type="submit">Submit Form</button>
-                <ContactButton />
-            </form>
+            <div className='form-body'>
+                <form onSubmit={onSubmit}>
+                    <label>Nome
+                        <input type="text" name="name" />
+                    </label>
+                    <label>Email
+                        <input type="email" name="email" />
+                    </label>
+                    <textarea name="message"></textarea>
+                    <button type="submit" className='btn-submit'>Enviar</button>
+                    <button onClick={SwitchScreen} className='back-btn'>Voltar</button>
+                    {/* <ContactButton /> */}
+                </form>
+            </div>
         );
     }
 
@@ -87,7 +90,8 @@ function Home() {
                         <h4>Designer</h4>
                     </div>
                     <div className="button-space">
-                        <ContactButton />
+                        <button onClick={SwitchScreen} className="contact-btn">Contate-me</button>
+                        {/* <ContactButton /> */}
                     </div>
                 </div>
                 <div className='my-img'>
