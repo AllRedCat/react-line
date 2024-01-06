0f12742a8c717e49dc8f357f8af52a1f7c3baaa8
guerson345 <leonismogabriel@gmail.com>
2024-01-06 01:25:44 -0300

    Ajust colors

--- portfolio-redcat/src/Services.tsx
+++ portfolio-redcat/src/Services.tsx
@@ -2,40 +2,42 @@ import './Services.scss'
 
 function Services() {
     return (
-        <div id="services" className="services-container">
-            <div className="name-container">
-                <h2>Serviços</h2>
+        <body>
+            <div id="services" className="services-container">
+                <div className="name-container">
+                    <h2>Serviços</h2>
+                </div>
+                <div className="services-items">
+                    <div className="services">
+                        <div className="img-place">
+                        </div>
+                        <div className="services-text">
+                            <h3>Design Gráfico</h3>
+                            <h4>Design para impulsionar o seu negócio no mercado</h4>
+                        </div>
+                    </div>
+                    <div className="services">
+                        <div className="img-place">
+                            <i className="bi bi-code-slash"></i>
+                        </div>
+                        <div className="services-text">
+                            <h3>UI/UX Design</h3>
+                            <h4>Design de interface de experiência de usuário
+                                para aplicações</h4>
+                        </div>
+                    </div>
+                    <div className="services">
+                        <div className="img-place">
+                        </div>
+                        <div className="services-text">
+                            <h3>WEB Design</h3>
+                            <h4>Design de paginas web para melhores sites para
+                                empresas e negócios</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="services-items">
-                <div className="services">
-                    <div className="img-place">
-                    </div>
-                    <div className="services-text">
-                        <h3>Design Gráfico</h3>
-                        <h4>Design para impulsionar o seu negócio no mercado</h4>
-                    </div>
-                </div>
-                <div className="services">
-                    <div className="img-place">
-                        <i className="bi bi-code-slash"></i>
-                    </div>
-                    <div className="services-text">
-                        <h3>UI/UX Design</h3>
-                        <h4>Design de interface de experiência de usuário
-                            para aplicações</h4>
-                    </div>
-                </div>
-                <div className="services">
-                    <div className="img-place">
-                    </div>
-                    <div className="services-text">
-                        <h3>WEB Design</h3>
-                        <h4>Design de paginas web para melhores sites para
-                            empresas e negócios</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </body>
     );
 }
 
